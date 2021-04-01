29b52961abff4b9f8e828960e0a3b48aea37ab70
GUERAUD-PINET Baptiste <baptiste.gueraud-pinet@laposte.net>
2021-04-01 14:22:30 +0200

    Projet React

---
+++ src/pages/About.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Logo from '../components/Logo';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Navigation />
+            <Logo />
+            <h1>À propos</h1>
+            <br/>
+            <p>Aute anim ut mollit Lorem consequat in est Lorem reprehenderit enim quis. Elit do ex in tempor ullamco ad sunt voluptate. 
+                Sit ex commodo labore exercitation dolore veniam ad ad qui consectetur.
+            </p>
+            <br/>
+            <p>Aute anim ut mollit Lorem consequat in est Lorem reprehenderit enim quis. Elit do ex in tempor ullamco ad sunt voluptate. 
+                Sit ex commodo labore exercitation dolore veniam ad ad qui consectetur.
+            </p>
+        </div>
+    );
+};
+
+export default About;
